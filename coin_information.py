22df70704e96c5fe5a3a7cfdb9f91cc73070d23d
Nisha Pant <nishap2@illinois.edu>
2021-10-13 23:16:01 -0500

    finished

--- coin_information.py
+++ coin_information.py
@@ -2,27 +2,38 @@ import pandas as pd
 from random import seed
 from random import randint
 
-df = pd.read_csv ('coin_Bitcoin.csv')
 
-bitcoin = []
+file_names = ['coin_Bitcoin.csv', 'coin_Cosmos.csv', 'coin_Dogecoin.csv', 'coin_Ethereum.csv',
+                'coin_Litecoin.csv', 'coin_Tether.csv', 'coin_USDCoin.csv', 'coin_Solana.csv']
 
-bitcoin_index = 435
-seed(1)
-for index, entry in df.iterrows():
-    date = entry['Date']
-    high = entry['High']
-    low = entry['Low']
-    open_ = entry['Open']
-    close = entry['Close']
-    volume = entry['Volume']
-    marketcap = entry['Marketcap']
-    coinid = bitcoin_index
-    sentiment = randint(-1, 1)
+indices = [435, 806, 1077, 1264, 1996, 3216, 3361, 3027]
+
+coin_info = []
 
 
-    bitcoin.append([date, open_, close, high, low, volume, marketcap, sentiment, coinid])
+for i in range(len(file_names)):
+    df = pd.read_csv (file_names[i])
 
-df2 = pd.DataFrame(bitcoin, columns=['TradingDate', 'TradingOpen', 'TradingClose', 'TradingHigh', 'TradingLow', 'TradingVolume', 
-'TradingMarketCap', 'OverallSentiment', 'CoinID'])
+    curr_index = indices[i]
+    seed(1)
+    for index, entry in df.iterrows():
+        date = entry['Date']
+        high = entry['High']
+        low = entry['Low']
+        open_ = entry['Open']
+        close = entry['Close']
+        volume = entry['Volume']
+        marketcap = entry['Marketcap']
+        coinid = curr_index
+        sentiment = randint(-1, 1)
+
+
+        coin_info.append([date, open_, close, high, low, volume, marketcap, sentiment, coinid])
+
+
+df2 = pd.DataFrame(coin_info, columns=['TradingDate', 'TradingOpen', 'TradingClose', 'TradingHigh', 'TradingLow', 'TradingVolume', 
+    'TradingMarketCap', 'OverallSentiment', 'CoinID'])
 
 print(df2)
+
+df2.to_csv('past_daily.csv')
